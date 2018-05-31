b087b6eac1ba09bd4548e533596fb7a9c5f04037
Alex <alex.r.clark21@gmail.com>
2018-05-30 20:21:09 -0600

    Basics set up

---
+++ src/router/routes.js
@@ -0,0 +1,6 @@
+import react from 'react'
+import {Route, Switch} from 'react-router-dom'
+
+<Switch>
+    
+</Switch>
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './ducks/reducer';
+
+
+export default createStore(reducer)
